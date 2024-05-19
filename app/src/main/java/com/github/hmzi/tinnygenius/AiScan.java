49d7024c09c318ade7d53430e4448c60f143fbd0
Muhammad Naveed <85978999+uxlabspk@users.noreply.github.com>
2024-05-19 19:32:15 +0500

    Naveed : adding ai scan screen image selector

--- app/src/main/java/com/github/hmzi/tinnygenius/AiScan.java
+++ app/src/main/java/com/github/hmzi/tinnygenius/AiScan.java
@@ -1,6 +1,10 @@
 package com.github.hmzi.tinnygenius;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,63 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.github.hmzi.tinnygenius.Classes.SpeakClass;
+import com.github.hmzi.tinnygenius.databinding.ActivityAiScanBinding;
+import com.github.hmzi.tinnygenius.databinding.ActivityAnimalBinding;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class AiScan extends AppCompatActivity {
+
+    private ActivityAiScanBinding binding;
+
+    // Uri indicates, where the image will be picked from
+    private Uri filePath;
+
+    // request code
+    private final int PICK_IMAGE_REQUEST = 22;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_ai_scan);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityAiScanBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        init();
+    }
+
+    private void init() {
+        // goBack
+        binding.back.setOnClickListener(view -> onBackPressed());
+
+        // on image select
+        binding.imgSelectBtn.setOnClickListener(view -> SelectImage());
+    }
+
+    // Select Image method
+    private void SelectImage() {
+        // Defining Implicit Intent to mobile gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Image from here..."), PICK_IMAGE_REQUEST);
+    }
+
+    // Override onActivityResult method
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            // Get the Uri of data
+            filePath = data.getData();
+            try {
+                // Setting image on image view using Bitmap
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                binding.userSelectedImage.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
